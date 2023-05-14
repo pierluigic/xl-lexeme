831ba7d8f44bd80f96236682f29c422c79fff3e3
pierluigic <pierluigicassotti@gmail.com>
2023-05-14 23:12:39 +0200

    Add files via upload

---
+++ InputExample.py
@@ -0,0 +1,26 @@
+from typing import Union, List
+
+
+class InputExample:
+    """
+    Structure for one input example with texts, the label and a unique id
+    """
+    def __init__(self, guid: str = '', texts = None,  positions = None, label: Union[int, float] = 0):
+        """
+        Creates one InputExample with the given texts, guid and label
+
+
+        :param guid
+            id for the example
+        :param texts
+            the texts for the example. Note, str.strip() is called on the texts
+        :param label
+            the label for the example
+        """
+        self.guid = guid
+        self.texts = texts
+        self.positions = positions
+        self.label = label
+
+    def __str__(self):
+        return "<InputExample> label: {}, texts: {}".format(str(self.label), "; ".join(self.texts))
